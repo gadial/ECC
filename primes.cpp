da7fcb9a895a7c0d0fd34afdb7dd9d1e7200fe1e
Gadi Aleksandrowicz <gadial@gadial-desktop.(none)>
2009-11-05 17:19:04 +0200

    Added function to generate random primes (based on existing Miller-Rabin implementation in gmp)

--- primes.cpp
+++ primes.cpp
@@ -1,6 +1,23 @@
+#include <gmp.h>
+#include <stdlib.h>
+#include <time.h>
 #include "primes.h"
 
-mpz_class generate_prime(mpz_class max){
-    mpz_class prime_number = 3; //3 is guaranteed to be prime
-    return prime_number;
+#include <stdio.h>
+#define MILLER_RABIN_REPEATS 5
+
+mpz_class generate_prime(unsigned long int n){
+    gmp_randstate_t random_state;
+    gmp_randinit_mt(random_state);
+    gmp_randseed_ui(random_state,time(NULL)); //for now, use a random seed
+    mpz_t temp;
+    mpz_init(temp);
+
+    //by the prime number theorem, 10*n attempts should do the trick
+    for (int i=0; i<10*n; i++){
+        mpz_urandomb(temp, random_state,n);
+        if (mpz_probab_prime_p(temp, MILLER_RABIN_REPEATS) != 0)
+            return mpz_class(temp);
+    }
+    return 1; //failure
 }
