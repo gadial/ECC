6f34e1445c69b6bdb5fc3209ac876db4fb60a1e4
gadial <gadial@gadial-desktop>
2009-11-05 11:02:33 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: gadial
+ *
+ * Created on November 5, 2009, 10:44 AM
+ */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=ecc
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/ecc
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=ecc.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/ecc.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=ecc
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/ecc
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=ecc.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/ecc.tar
